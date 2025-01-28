a7c00d3e6fcc8bd4ec9b4ce39aa0ad276598111f
MillYoo <mill0452130@gmail.com>
2025-01-29 01:36:03 +0900

    css 수정 및 데이터 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ import type { AppProps } from "next/app";
 const DEFAULT_SEO = {
   title: "유밀 | Full-Stack Dev",
   description: "안녕하세요, 풀스택 개발자 유밀입니다.",
-  canonical: "https://www.naver.com/",
+  canonical: "https://portfolio-millyoo.vercel.app/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
-    url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    url: "https://portfolio-millyoo.vercel.app/",
+    title: "유밀 | Full-Stack Dev",
+    site_name: "유밀 | Full-Stack Dev",
     images: [
       {
         url: "/share.png",
